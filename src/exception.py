f80be6bb6bd6342ba80f0f18cfdcf5308d365f3d
Parthiv Shah <parthivshah33@gmail.com>
2024-07-14 01:45:26 +0530

    Logging, Exception functionality added

---
+++ src/exception.py
@@ -0,0 +1,34 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    """
+    Args:
+        error (_type_): _description_
+        error_detail (sys): _description_
+    """
+
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    error_message = "Error occured in python script name [{0}] line number [{1}] Error message [{2}]".format(
+        file_name,
+        line_number,
+        str(error)
+
+    )
+
+    return error_message
+
+
+class CustomeException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error=error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
